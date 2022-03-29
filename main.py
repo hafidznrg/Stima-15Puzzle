ff969b4c3aa3d62f1b0c723f5ac04ff3dca2e22d
hafidznrg <hafidznrg.edu@gmail.com>
2022-03-29 15:32:33 +0700

    feat: finish algorithm, finally work

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from tkinter import S
 from masukan import getInput
 from enum import Enum
 
@@ -9,22 +8,23 @@ class Direction(Enum):
     RIGHT = 4
 
 class Node:
-    def __init__(self, matriks, route = [], parent = None, depth = 0, cost = 0):
+    def __init__(self, matriks, route = [], parent = None, depth = 0):
         self.matriks = matriks
         self.route = route
         self.parent = parent
         self.depth = depth
-        self.cost = cost
+        self.cost = self.calculateCost()
         self.x, self.y = self.getZero()
     
-    # def calculateCost(self):
-    #     cost = 0
-    #     for i in range(16):
-    #         if (self.matriks[i // 4][i % 4] != 0 and self.matriks[i // 4][i % 4] != i + 1):
-    #             cost += 1
-    #     if (self.parent != None):
-    #         cost += self.parent.cost
-    #     return cost
+    def calculateCost(self):
+        if self.depth == 0:
+            return 0
+        cost = 0
+        for i in range(16):
+            if (self.matriks[i // 4][i % 4] != 0 and self.matriks[i // 4][i % 4] != i + 1):
+                cost += 1
+        cost += self.depth
+        return cost
     
     def print(self):
         print("Route: ", self.route)
@@ -35,7 +35,7 @@ class Node:
     
     def isGoal(self):
         isGoal = True
-        for i in range(16):
+        for i in range(15):
             if (self.matriks[i // 4][i % 4] != i + 1):
                 isGoal = False
                 break
@@ -77,6 +77,39 @@ class Node:
                 children.append(Node(child, self.route + [direction], self, self.depth + 1))
         return children
 
+class QueueNode:
+    def __init__(self):
+        self.queue = []
+
+    def push(self, node):
+        if (self.length() == 0):
+            self.queue.append(node)
+        else:
+            for i in range(self.length()):
+                if (node.cost < self.queue[i].cost):
+                    self.queue.insert(i, node)
+                    break
+                elif (i == self.length() - 1):
+                    self.queue.append(node)
+
+    def pop(self):
+        return self.queue.pop(0)
+    
+    def length(self):
+        return len(self.queue)
+
+    def kill(self, cost):
+        i = 0
+        while (i < self.length()):
+            if (self.queue[i].cost > cost):
+                self.queue.pop(i)
+            else:
+                i += 1
+
+    def print(self):
+        for node in self.queue:
+            node.print()
+
 def hitungKurang(matriks):
     row = 0
     col = 0
@@ -110,34 +143,46 @@ def run(matriks):
     # check Kurang(i) + X
     sumKurang = hitungKurang(matriks)
 
+    print("Total : " + str(sumKurang))
+
     # jika Kurang(i) + X ganjil makan tidak ada solusi
     if (sumKurang%2 == 1):
-        print("Tidak dapat mencapai solusi")
+        print("Puzzle tidak dapat diselesaikan")
         return
     
     # Nasuk ke algoritma Branch and Bound
-    queue = []
-    queue.append(Node(matriks))
+    queue = QueueNode()
+    queue.push(Node(matriks))
+    banyakSimpul = 1
     solution = None
 
     # Masuk perulangan
     print("Masuk perulangan")
-    while len(queue) > 0:
-        node = queue.pop(0)
-        node.print()
+    while queue.length() > 0:
+        node = queue.pop()
         if node.isGoal():
-            print("Ditemukan sebuah solusi")
             solution = node
-            # Masih perlu mematikan simpul yang memiliki cost lebih besar
+            queue.kill(node.cost)
         else:
             child = node.getChildren()
+            banyakSimpul += len(child)
             for i in range(len(child)):
-                child[i].cost = calculateCost(child[i].matriks)
-                queue.append(child[i])
-                print("Anak ke-" + str(i) + ":")
-                child[i].print()
-            
+                queue.push(child[i])
     
+    rute = []
+    if (solution != None):
+        rute.append(solution)
+        print("Banyak simpul yang dibangkitkan: " + str(banyakSimpul))
+        # print("Rute yang digunakan: " + str(solution.route))
+        iterator = solution.parent
+        while (iterator != None):
+            rute.insert(0, iterator)
+            iterator = iterator.parent
+
+    print("=============")
+    for i in range(len(rute)):
+        printMatriks(rute[i].matriks)
+        print("=============")
 
 def printMatriks(matriks):
     for line in matriks:
@@ -146,9 +191,6 @@ def printMatriks(matriks):
         print()
 
 matriks = getInput()
-# print("Matriks awal:")
-# printMatriks(matriks)
+print("Matriks awal:")
+printMatriks(matriks)
 run(matriks)
-
-# for direction in Direction:
-#     print(direction)
